7503fa6c0ee0c5cfb92e35cfa0259a45ed59d858
David Burela <David.Burela@gmail.com>
2014-01-30 02:58:28 +1100

    Implemented IoC with AutoFac

--- TodoListWeb/Global.asax.cs
+++ TodoListWeb/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using TodoListWeb.Data.Repositories;
+using TodoListWeb.Data.Uow;
 
 namespace TodoListWeb
 {
@@ -12,6 +14,14 @@ namespace TodoListWeb
     {
         protected void Application_Start()
         {
+            // Configure Autofac
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<TodoItemRepository>().As<ITodoItemRepository>();
+            builder.RegisterType<TodoListUow>();
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
