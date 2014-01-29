23b2aada9e6c6bbedb93bc4e732d2b6c24289b6a
David Burela <David.Burela@gmail.com>
2014-01-30 02:07:20 +1100

    Initial commit
    Create a new MVC 5 project with unit tests

---
+++ TodoListWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TodoListWeb.Startup))]
+namespace TodoListWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
