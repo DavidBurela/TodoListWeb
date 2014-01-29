f24c3df836d763fcdb9f309a9c40ce2b2ebe373f
David Burela <David.Burela@gmail.com>
2014-01-30 02:11:49 +1100

    Added model & EF code first DB context
    Create the TodoItem model to hold the data for the system.
    Created the TodoListDbContext which will automatically generate the
    database & tables.

---
+++ TodoListWeb/Data/TodoListDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using TodoListWeb.Models;
+
+namespace TodoListWeb.Data
+{
+    /// <summary>
+    /// Entity framework code first Db Context.
+    /// This will automatically generate the database & tables for me.
+    /// </summary>
+    public class TodoListDbContext: DbContext
+    {
+        // Automatically use the connection string in the web.config
+        public TodoListDbContext()
+            : base("DefaultConnection") { }
+
+        // Tables
+        public DbSet<TodoItem> TodoItems { get; set; }
+ }
---
+++ TodoListWeb/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoListWeb.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+    }
+}
