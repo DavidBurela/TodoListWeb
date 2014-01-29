05043568ae2473a95c5b08ebed01cd9bce6254ec
David Burela <David.Burela@gmail.com>
2014-01-30 03:01:10 +1100

    Added JQuery UI and updated Script/Style Bundles

--- TodoListWeb/App_Start/BundleConfig.cs
+++ TodoListWeb/App_Start/BundleConfig.cs
@@ -11,8 +11,11 @@ namespace TodoListWeb
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -25,7 +28,8 @@ namespace TodoListWeb
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/themes/base/jquery-ui.css"));
         }
     }
 }
